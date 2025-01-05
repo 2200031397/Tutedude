27d3607bebb4d27fc93659c3acd8142eb37c6eef
2200031397 <139319227+2200031397@users.noreply.github.com>
2025-01-05 11:08:04 +0530

    Update Login

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -18,7 +18,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:3000/api/login', {
+      const response = await axios.post('https://tutedude-backend-1.onrender.com/api/login', {
         name,
         password,
       });
